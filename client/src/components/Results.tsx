1cd7e952515b70dec1439283c97912f87f2179ff
Eden Meeks <eden@edensshopifymbp.lan>
2022-12-06 09:22:39 -0500

    new

--- client/src/components/Results.tsx
+++ client/src/components/Results.tsx
@@ -4,10 +4,13 @@ const Results = ({results, onAddNom, nomListItems}) => {
   if (results.length <1 ) {
     return <p> no results</p>
   }
-  const resultLI = results.map((result) => 
-  <li key={result.imdbID}>{result.title}
-    <button onClick={() => onAddNom(result)}> Nominate</button></li>
-  );
+  const resultLI = results.map((result) => {
+    return (
+      <li key={result.imdbID}>{result.title}{result.year}
+        <button onClick={() => onAddNom(result)} disabled={nomListItems.includes(result)}> Nominate</button>
+      </li>
+    );
+  });
   return (
     <>
       <h3>Results</h3>
